93ae7118c8da7194f3a52c8ac9e5498ef4b7434e
fulltechstack <fulltechstack@gmail.com>
2024-12-23 18:27:22 +0530

    Add project files.

---
+++ S_SingleResponsibilityPrinciple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SOLID_Principles
+{
+    internal class S_SingleResponsibilityPrinciple
+    {
+    }
+
+    public class Chef
+    {
+        public void PrepareFood()
+        {
+            Console.WriteLine("Cooking food...");
+        }
+
+    }
+
+    public class Receptionist
+    {
+        public void ManageBookings()
+        {
+            Console.WriteLine("Manage table bookings...");
+        }
+    }
+}
