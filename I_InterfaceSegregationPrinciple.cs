39c61616a5650b7f34215531b9420048cd99d4f8
fulltechstack <fulltechstack@gmail.com>
2024-12-26 11:07:52 +0530

    Interface segregation principle

---
+++ I_InterfaceSegregationPrinciple.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SOLID_Principles
+{
+    public class I_InterfaceSegregationPrinciple
+    {
+    }
+
+    //public interface IHotelServices
+    //{
+    //    void BookRoom();
+    //    void OrderFood();
+    //    void ManageEvent();
+    //}
+
+    public interface IRoomBookingService
+    {
+        void BookRoom();
+    }
+    public interface IFoodOrderingService
+    {
+        void OrderFood();
+    }
+    public interface IEventManagementService
+    {
+        void ManageEvent();
+    }
+
+    public class RegularGuest : IRoomBookingService
+    {
+        public void BookRoom()
+        {
+            Console.WriteLine("Room booked successfully");
+        }
+    }
+
+    public class EventClient : IEventManagementService
+    {
+        public void ManageEvent()
+        {
+            Console.WriteLine("Event managed successfully");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IRoomBookingService regularGuest = new RegularGuest();
+            regularGuest.BookRoom();
+
+            IEventManagementService eventClient = new EventClient();
+            eventClient.ManageEvent();
+        }
+    }
+}
